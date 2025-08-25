8de9637836621845f41ad189889864490839fab2
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2025-08-25 16:34:27 +0800

    Create P1001 A+B Problem.java

---
+++ P1001 A+B Problem.java
@@ -0,0 +1,15 @@
+//1. 導入包
+import java.util.Scanner;
+
+public class Main {
+
+     public static void main(String[] args) {
+     //2. 創建對象
+        Scanner sc = new Scanner(System.in);
+        //3. 接收數據
+        int number1 = sc.nextInt(), number2 = sc.nextInt();
+        //4. 打印結果
+        System.out.println(number1 + number2);
+
+     }
+}
