d4888d281066c406d770655c8c134c247fdab190
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2025-09-02 16:42:16 +0800

    Create fastIO2.java

---
+++ fastIO/按行輸入直到EOF/fastIO2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+/*
+ import java.io.BufferedReader;
+ import java.io.IOException;
+ import java.io.InputStreamReader;
+ import java.io.OutputStreamWriter;
+ import java.io.PrintWriter;
+ import java.util.StringTokenizer;
+ */
+
+public class fastIO2 {
+
+    public static String line;
+    public static String[] parts;
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        /*
+        while ((line = in.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+            while (st.hasMoreTokens()) {
+                out.println(st.nextToken());
+            }
+        }
+        */
+        while ((line = in.readLine()) != null) {
+            parts = line.trim().split("\\s+");
+            for (String part : parts) {
+                out.println(part);
+            }
+        }
+        out.flush();
+        out.close();
+        in.close();
+    }
+}
